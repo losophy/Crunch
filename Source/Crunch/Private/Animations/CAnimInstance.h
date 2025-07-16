cce92066218e38f6e3201c98a4e1a619a5c190fb
losophy <losophyl@gamil.com>
2025-07-16 21:10:34 +0800

    10.10 Create the Animation Instance

---
+++ Source/Crunch/Private/Animations/CAnimInstance.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	// the below functions are the native overrides for each phase
+	// Native initialization override point
+	virtual void NativeInitializeAnimation() override;
+	// Native update override point. It is usually a good idea to simply gather data in this step and 
+	// for the bulk of the work to be done in NativeThreadSafeUpdateAnimation.
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	// Native thread safe update override point. Executed on a worker thread just prior to graph update 
+	// for linked anim instances, only called when the hosting node(s) are relevant
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY()
+	class ACharacter* OwnerCharacter;
+
+	UPROPERTY()
+	class UCharacterMovementComponent* OwnerMovementComp;
+};
