97f12980c9ea86606097fb2897ff84ac353bac5b
losophy <losophyl@gamil.com>
2025-07-18 10:25:22 +0800

    18.18 Add the Ability System Components

--- Source/Crunch/Private/Character/CCharacter.cpp
+++ Source/Crunch/Private/Character/CCharacter.cpp
@@ -2,7 +2,9 @@
 
 
 #include "Character/CCharacter.h"
-#include"Components/SkeletalMeshComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GAS/CAbilitySystemComponent.h"
+#include "GAS/CAttributeSet.h"
 
 // Sets default values
 ACCharacter::ACCharacter()
@@ -10,6 +12,9 @@ ACCharacter::ACCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	GetMesh()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	CAbilitySystemComponent = CreateDefaultSubobject<UCAbilitySystemComponent>("CAbility System Component");
+	CAttributeSet = CreateDefaultSubobject<UCAttributeSet>("CAttribute Set");
 }
 
 // Called when the game starts or when spawned
@@ -31,5 +36,10 @@ void ACCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+UAbilitySystemComponent* ACCharacter::GetAbilitySystemComponent() const
+{
+	return CAbilitySystemComponent;
 }
 
