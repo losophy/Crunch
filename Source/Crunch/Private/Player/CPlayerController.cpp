49b6b2e57a56a189cb5c34c23f505c515fadb5f3
losophy <losophyl@gamil.com>
2025-07-20 23:40:10 +0800

    25.25 Add the Gameplay Widget Blueprint

--- Source/Crunch/Private/Player/CPlayerController.cpp
+++ Source/Crunch/Private/Player/CPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "Player/CPlayerController.h"
 #include "Player/CPlayerCharacter.h"
+#include "Widgets/GameplayWidget.h"
 
 void ACPlayerController::OnPossess(APawn* NewPawn)
 {
@@ -21,5 +22,18 @@ void ACPlayerController::AcknowledgePossession(APawn* NewPawn)
 	if (CPlayerCharacter)
 	{
 		CPlayerCharacter->ClientSideInit();
+		SpawnGameplayWidget();
+	}
+}
+
+void ACPlayerController::SpawnGameplayWidget()
+{
+	if (!IsLocalPlayerController())
+		return;
+
+	GameplayWidget = CreateWidget<UGameplayWidget>(this, GameplayWidgetClass);
+	if (GameplayWidget)
+	{
+		GameplayWidget->AddToViewport();
 	}
 }
