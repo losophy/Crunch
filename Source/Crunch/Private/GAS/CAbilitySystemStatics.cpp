55dbfc14ec219d34023d94f449a0919da9bff419
losophy <losophyl@gamil.com>
2025-07-22 15:11:46 +0800

    32.32 Prevent the Combo From Firing By Using Gameplay Tags

---
+++ Source/Crunch/Private/GAS/CAbilitySystemStatics.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/CAbilitySystemStatics.h"
+
+FGameplayTag UCAbilitySystemStatics::GetBasicAttackAbilityTag()
+{
+	return FGameplayTag::RequestGameplayTag("ability.basicattack");
+}
