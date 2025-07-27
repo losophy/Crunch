5bb26948d39698e2e832bd8ae7c4ed993fd1a6a6
losophy <losophyl@gamil.com>
2025-07-27 15:51:08 +0800

    55.55 Initialize Team ID and Start Spot for Players

--- Source/Crunch/Private/Framework/CGameMode.cpp
+++ Source/Crunch/Private/Framework/CGameMode.cpp
@@ -2,7 +2,49 @@
 
 
 #include "Framework/CGameMode.h"
+#include "GameFramework/PLayerStart.h"
+#include "EngineUtils.h"
 
+APlayerController* ACGameMode::SpawnPlayerController(ENetRole InRemoteRole, const FString& Options)
+{
+	APlayerController* NewPlayerController = Super::SpawnPlayerController(InRemoteRole, Options);
+	IGenericTeamAgentInterface* NewPlayerTeamInterface = Cast<IGenericTeamAgentInterface>(NewPlayerController);
+	FGenericTeamId TeamId = GetTeamIDForPlayer(NewPlayerController);
+	if (NewPlayerTeamInterface)
+	{
+		NewPlayerTeamInterface->SetGenericTeamId(TeamId);
+	}	
 
+	NewPlayerController->StartSpot = FindNextStartSpotForTeam(TeamId);
 
+	return NewPlayerController;
+}
 
+FGenericTeamId ACGameMode::GetTeamIDForPlayer(const APlayerController* PlayerController) const
+{
+	static int PlayerCount = 0;
+	++PlayerCount;
+	return FGenericTeamId(PlayerCount % 2);
+}
+
+AActor* ACGameMode::FindNextStartSpotForTeam(const FGenericTeamId& TeamID) const
+{
+	const FName* StartSpotTag = TeamStartSoptTagMap.Find(TeamID);
+	if (!StartSpotTag)
+	{
+		return nullptr;
+	}
+
+	UWorld* World = GetWorld();
+
+	for (TActorIterator<APlayerStart> It(World); It; ++It)
+	{
+		if (It->PlayerStartTag == *StartSpotTag)
+		{
+			It->PlayerStartTag = FName("Taken");
+			return *It;
+		}
+	}
+
+	return nullptr;
+}
--- Source/Crunch/Private/Framework/CGameMode.h
+++ Source/Crunch/Private/Framework/CGameMode.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "GenericTeamAgentInterface.h"
 #include "CGameMode.generated.h"
 
 /**
@@ -13,8 +14,14 @@ UCLASS()
 class ACGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+public:
+	virtual APlayerController* SpawnPlayerController(ENetRole InRemoteRole, const FString& Options) override;
+
+private:
+	FGenericTeamId GetTeamIDForPlayer(const APlayerController* PlayerController) const;
+
+	AActor* FindNextStartSpotForTeam(const FGenericTeamId& TeamID) const;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Team")
+	TMap<FGenericTeamId, FName> TeamStartSoptTagMap;
 };
