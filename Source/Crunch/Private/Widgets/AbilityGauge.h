87b80e348f337338c542fec3867cd9261a2bcd11
losophy <losophyl@gamil.com>
2025-08-07 17:22:40 +0800

    91.91 Use Data Table to Store Ability Icon

--- Source/Crunch/Private/Widgets/AbilityGauge.h
+++ Source/Crunch/Private/Widgets/AbilityGauge.h
@@ -7,6 +7,23 @@
 #include "Blueprint/IUserObjectListEntry.h"
 #include "AbilityGauge.generated.h"
 
+USTRUCT(BlueprintType)
+struct FAbilityWidgetData : public FTableRowBase
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<class UGameplayAbility> AbilityClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName AbilityName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UTexture2D> Icon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Description;
+};
+
 /**
  * 
  */
@@ -16,7 +33,11 @@ class UAbilityGauge : public UUserWidget, public IUserObjectListEntry
 	GENERATED_BODY()
 public:
 	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
+	void ConfigureWithWidgetData(const FAbilityWidgetData* WidgetData);
 private:
+	UPROPERTY(EditDefaultsOnly, Category = "Visual")
+	FName IconMaterialParamName = "Icon";
+
 	UPROPERTY(meta = (BindWidget))
 	class UImage* Icon;
 
--- Source/Crunch/Private/Widgets/AbilityListView.h
+++ Source/Crunch/Private/Widgets/AbilityListView.h
@@ -16,4 +16,12 @@ class UAbilityListView : public UListView
 	GENERATED_BODY()
 public:
 	void ConfigureAbilities(const TMap<ECAbilityInputID, TSubclassOf<class UGameplayAbility>>& Abilities);
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Data")
+	UDataTable* AbilityDataTable;
+
+	void AbilityGaugeGenerated(UUserWidget& Widget);
+
+	const struct FAbilityWidgetData* FindWidgetDataForAbility(const TSubclassOf<UGameplayAbility>& AbilityClass) const;
 };
