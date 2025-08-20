731f4552faf87ba8e0fa29fc100d31c059ab5e95
losophy <losophyl@gamil.com>
2025-08-20 14:07:35 +0800

    130.130 Refactor AN_SendTargetGroup to Send Damage GameplayCue Locally

--- Source/Crunch/Private/Animations/AN_SendTargetGroup.h
+++ Source/Crunch/Private/Animations/AN_SendTargetGroup.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Animation/AnimNotifies/AnimNotify.h"
 #include "GameplayTagContainer.h"
+#include "GenericTeamAgentInterface.h"
 #include "AN_SendTargetGroup.generated.h"
 
 /**
@@ -19,7 +20,25 @@ public:
 
 private:
 	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	FGameplayTagContainer TriggerGameplayCueTags;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	TEnumAsByte<ETeamAttitude::Type> TargetTeam = ETeamAttitude::Hostile;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	float SphereSweepRadius = 60.f;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	bool bDrawDebug = true;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	bool bIgnoreOwner = true;
+
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
 	FGameplayTag EventTag;
+
 	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
 	TArray<FName> TargetSocketNames;
+
+	void SendLocalGameplayCue(const FHitResult& HitResult) const;
 };
