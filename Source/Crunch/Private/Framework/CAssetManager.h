0a1bd58f2379118f1c433d1c04234042847a5be6
losophy <losophyl@gamil.com>
2025-08-21 15:55:48 +0800

    134.134 Create the Asset Manager and ShopItem Loading Functions

---
+++ Source/Crunch/Private/Framework/CAssetManager.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "Inventory/PA_ShopItem.h"
+#include "CAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+public:
+	static UCAssetManager& Get();
+	void LoadShopItems(const FStreamableDelegate& LoadFinishedCallback);
+	bool GetLoadedShopItems(TArray<const UPA_ShopItem*>& OutItems) const;
+private:
+	void ShopItemLoadFinished(FStreamableDelegate Callback);
+	void BuildItemMaps();
+	void AddToCombinationMap(const UPA_ShopItem* Ingredient, const UPA_ShopItem* CombinationItem);
+
+	UPROPERTY()
+	TMap<const UPA_ShopItem*, FItemCollection> CombinationMap;
+	UPROPERTY()
+	TMap<const UPA_ShopItem*, FItemCollection> IngredientMap;
+};
