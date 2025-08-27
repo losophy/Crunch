31d818cf221386bb313d19b3220c8f144fb72798
losophy <losophyl@gamil.com>
2025-08-27 15:40:29 +0800

    145.145 Implement the Inventory Item Widget

--- Source/Crunch/Private/Inventory/InventoryItem.cpp
+++ Source/Crunch/Private/Inventory/InventoryItem.cpp
@@ -55,6 +55,16 @@ uint32 GetTypeHash(const FInventoryItemHandle& Key)
 	return Key.GetHandleId();
 }
 
+UInventoryItem::UInventoryItem()
+	:StackCount{ 1 }
+{
+}
+
+bool UInventoryItem::IsValid() const
+{
+	return ShopItem != nullptr;
+}
+
 void UInventoryItem::InitItem(const FInventoryItemHandle& NewHandle, const UPA_ShopItem* NewShopItem)
 {
 	Handle = NewHandle;
@@ -88,4 +98,9 @@ void UInventoryItem::ApplyGASModifications(UAbilitySystemComponent* AbilitySyste
 			GrantedAbiltiySpecHandle = AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(GrantedAbility));
 		}
 	}
+}
+
+void UInventoryItem::SetSlot(int NewSlot)
+{
+	Slot = NewSlot;
 }
