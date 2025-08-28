ef23a04c5b4a5efbda414d92c6329759108525b6
losophy <losophyl@gamil.com>
2025-08-28 10:07:51 +0800

    147.147 Create the Inventory Widget Blueprint

--- Source/Crunch/Private/Inventory/InventoryComponent.cpp
+++ Source/Crunch/Private/Inventory/InventoryComponent.cpp
@@ -103,6 +103,9 @@ void UInventoryComponent::Server_Purchase_Implementation(const UPA_ShopItem* Ite
 	if (GetGold() < ItemToPurchase->GetPrice())
 		return;
 
+	if (InventoryMap.Num() >= GetCapacity())
+		return;
+
 	OwnerAbilitySystemComponent->ApplyModToAttribute(UCHeroAttributeSet::GetGoldAttribute(), EGameplayModOp::Additive, -ItemToPurchase->GetPrice());
 	GrantItem(ItemToPurchase);
 }
--- Source/Crunch/Private/Widgets/GameplayWidget.h
+++ Source/Crunch/Private/Widgets/GameplayWidget.h
@@ -47,6 +47,9 @@ private:
 	UPROPERTY(meta = (BindWidget))
 	class UShopWidget* ShopWidget;
 
+	UPROPERTY(meta = (BindWidget))
+	class UInventoryWidget* InventoryWidget;
+
 	UPROPERTY(Transient, meta = (BindWidgetAnim))
 	class UWidgetAnimation* ShopPopupAnimation;
 
