b8d4f393550e6bd46401b5199f1b09f149b18b8d
losophy <losophyl@gamil.com>
2025-09-01 09:05:25 +0800

    155.155 Create The Context Menu class and Implement the Spawning Logic

--- Source/Crunch/Private/Widgets/InventoryWidget.cpp
+++ Source/Crunch/Private/Widgets/InventoryWidget.cpp
@@ -4,6 +4,7 @@
 #include "Widgets/InventoryWidget.h"
 #include "Inventory/InventoryComponent.h"
 #include "Widgets/InventoryItemWidget.h"
+#include "Widgets/InventoryContextMenuWidget.h"
 #include "Components/WrapBox.h"
 #include "Components/WrapBoxSlot.h"
 
@@ -34,11 +35,50 @@ void UInventoryWidget::NativeConstruct()
 
 					NewEmptyWidget->OnInventoryItemDropped.AddUObject(this, &UInventoryWidget::HandleItemDragDrop);
 					NewEmptyWidget->OnLeftBttonClicked.AddUObject(InventoryComponent, &UInventoryComponent::TryActivateItem);
+					NewEmptyWidget->OnRightBttonClicked.AddUObject(this, &UInventoryWidget::ToggleContextMenu);
 				}
 			}
-
+			SpawnContextMenu();
 		}
 	}
+}
+
+void UInventoryWidget::SpawnContextMenu()
+{
+	if (!ContextMenuWidgetClass)
+		return;
+
+	ContextMenuWidget = CreateWidget<UInventoryContextMenuWidget>(this, ContextMenuWidgetClass);
+	if (ContextMenuWidget)
+	{
+		ContextMenuWidget->GetSellButtonClickedEvent().AddDynamic(this, &UInventoryWidget::SellFocusedItem);
+		ContextMenuWidget->GetUseButtonClickedEvent().AddDynamic(this, &UInventoryWidget::UseFocusedItem);
+		ContextMenuWidget->AddToViewport(1);
+		SetContextMenuVisible(false);
+	}
+}
+
+void UInventoryWidget::SellFocusedItem()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Selling Item"));
+}
+
+void UInventoryWidget::UseFocusedItem()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Using Item"));
+}
+
+void UInventoryWidget::SetContextMenuVisible(bool bContextMenuVisible)
+{
+	if (ContextMenuWidget)
+	{
+		ContextMenuWidget->SetVisibility(bContextMenuVisible ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
+	}
+}
+
+void UInventoryWidget::ToggleContextMenu(const FInventoryItemHandle& ItemHandle)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Trying to toggle context menu"));
 }
 
 void UInventoryWidget::ItemAdded(const UInventoryItem* InventoryItem)
