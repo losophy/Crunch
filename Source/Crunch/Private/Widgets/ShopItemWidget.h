89347469f49d5114020cfbb682b2a36e90620345
losophy <losophyl@gamil.com>
2025-09-02 11:24:33 +0800

    160.160 Make the TreeNodeInterface and Implement It with ShopItemWidget

--- Source/Crunch/Private/Widgets/ShopItemWidget.h
+++ Source/Crunch/Private/Widgets/ShopItemWidget.h
@@ -3,8 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Widgets/ItemWidget.h"
 #include "Blueprint/IUserObjectListEntry.h"
+#include "Widgets/ItemWidget.h"
+#include "Widgets/TreeNodeInterface.h"
 #include "ShopItemWidget.generated.h"
 
 class UPA_ShopItem;
@@ -16,19 +17,31 @@ DECLARE_MULTICAST_DELEGATE_OneParam(FOnShopItemSelected, const UShopItemWidget*)
  * 
  */
 UCLASS()
-class UShopItemWidget : public UItemWidget, public IUserObjectListEntry
+class UShopItemWidget : public UItemWidget, public IUserObjectListEntry, public ITreeNodeInterface
 {
 	GENERATED_BODY()
 public:
 	FOnItemPurchaseIssused OnItemPurchaseIssued;
 	FOnShopItemSelected OnShopItemClicked;
 
+	virtual UUserWidget* GetWidget() const override;
+	virtual TArray<const ITreeNodeInterface*> GetInputs() const override;
+	virtual TArray<const ITreeNodeInterface*> GetOuputs() const override;
+	virtual const UObject* GetItemObject() const override;
+
 	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
 	FORCEINLINE const UPA_ShopItem* GetShopItem() const { return ShopItem; }
 
 private:
+	void CopyFromOther(const UShopItemWidget* OtherWidget);
+	void InitWithShopItem(const UPA_ShopItem* NewShopItem);
+	TArray<const ITreeNodeInterface*> ItemsToInterfaces(const TArray<const UPA_ShopItem*>& Items) const;
+
 	UPROPERTY()
 	const UPA_ShopItem* ShopItem;
+
+	const class UListView* ParentListView;
+
 	virtual void RightButtonClicked() override;
 	virtual void LeftButtonClicked() override;
 };
