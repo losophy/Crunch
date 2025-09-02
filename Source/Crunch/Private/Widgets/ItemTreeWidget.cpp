308f242cfefc51e7b08a70c8743261f3b20895a6
losophy <losophyl@gamil.com>
2025-09-02 15:03:38 +0800

    161.161 Create the Parameters and Utility Functions of the Item Tree Widget

--- Source/Crunch/Private/Widgets/ItemTreeWidget.cpp
+++ Source/Crunch/Private/Widgets/ItemTreeWidget.cpp
@@ -2,4 +2,49 @@
 
 
 #include "Widgets/ItemTreeWidget.h"
+#include "Widgets/SplineWidget.h"
+#include "Components/CanvasPanel.h"
+#include "Components/CanvasPanelSlot.h"
 
+void UItemTreeWidget::ClearTree()
+{
+	RootPanel->ClearChildren();
+}
+
+UUserWidget* UItemTreeWidget::CreateWidgetForNode(const ITreeNodeInterface* Node, UCanvasPanelSlot*& OutCanvasSlot)
+{
+	if (!Node)
+		return nullptr;
+
+	UUserWidget* NodeWidget = Node->GetWidget();
+	OutCanvasSlot = RootPanel->AddChildToCanvas(NodeWidget);
+	if (OutCanvasSlot)
+	{
+		OutCanvasSlot->SetSize(NodeSize);
+		OutCanvasSlot->SetAnchors(FAnchors(0.5f));
+		OutCanvasSlot->SetAlignment(FVector2D(0.5f));
+		OutCanvasSlot->SetZOrder(1);
+	}
+
+	return NodeWidget;
+}
+
+void UItemTreeWidget::CreateConnection(const UUserWidget* From, UUserWidget* To)
+{
+	if (!From || !To)
+		return;
+
+	USplineWidget* Connection = CreateWidget<USplineWidget>(GetOwningPlayer());
+	UCanvasPanelSlot* ConnectionPanelSlot = RootPanel->AddChildToCanvas(Connection);
+
+	if (ConnectionPanelSlot)
+	{
+		ConnectionPanelSlot->SetAnchors(FAnchors{ 0.f });
+		ConnectionPanelSlot->SetAlignment(FVector2D{ 0.f });
+		ConnectionPanelSlot->SetPosition(FVector2D::Zero());
+		ConnectionPanelSlot->SetZOrder(0);
+	}
+
+	Connection->SetupSpline(From, To, SourePortLocalPos, DestinationPortLocalPos, SourcePortDirection, DestinationPortDirection);
+	Connection->SetSplineStyle(ConnectionColor, ConnectionThickness);
+}
--- Source/Crunch/Private/Widgets/ItemTreeWidget.h
+++ Source/Crunch/Private/Widgets/ItemTreeWidget.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Widgets/TreeNodeInterface.h"
 #include "ItemTreeWidget.generated.h"
 
 /**
@@ -13,5 +14,35 @@ UCLASS()
 class UItemTreeWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+private:
+	void ClearTree();
+	UUserWidget* CreateWidgetForNode(const ITreeNodeInterface* Node, class UCanvasPanelSlot*& OutCanvasSlot);
+	void CreateConnection(const UUserWidget* From, UUserWidget* To);
+
+	UPROPERTY(meta = (BindWidget))
+	class UCanvasPanel* RootPanel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D NodeSize = FVector2D{ 60.f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D NodeGap = FVector2D{ 16.f, 30.f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FLinearColor ConnectionColor = FLinearColor{ 0.8f, 0.8f, 0.8f, 1.f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	float ConnectionThickness = 3.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D SourePortLocalPos = FVector2D{ 0.5f, 0.9f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D DestinationPortLocalPos = FVector2D{ 0.5f, 0.1f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D SourcePortDirection = FVector2D{ 0.f, 90.f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D DestinationPortDirection = FVector2D{ 0.f, 90.f };
 };
