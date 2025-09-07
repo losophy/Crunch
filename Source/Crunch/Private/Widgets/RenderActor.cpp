0cef591cd011ce5b0238c6d6371914058e9b637f
losophy <losophyl@gamil.com>
2025-09-07 13:23:53 +0800

    176.176 Setup the Lighting of the Headshot Render

--- Source/Crunch/Private/Widgets/RenderActor.cpp
+++ Source/Crunch/Private/Widgets/RenderActor.cpp
@@ -38,5 +38,5 @@ void ARenderActor::BeginPlay()
 {
 	Super::BeginPlay();
 	CaptureComponent->ShowOnlyActorComponents(this);
-	SetActorLocation(FVector{ 0.f, 100000.f, 0.f });
+	SetActorLocation(FVector{ 0.f, 0.f, 100000.f });
 }
