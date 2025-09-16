89b1312dc0642482a43ffc6bbcf5c4835f0eb3dd
losophy <losophyl@gamil.com>
2025-09-16 15:15:44 +0800

    196.196 Make the Crosshair Widget Blueprint

--- Source/Crunch/Private/Widgets/GameplayWidget.h
+++ Source/Crunch/Private/Widgets/GameplayWidget.h
@@ -74,6 +74,9 @@ private:
 	UPROPERTY(meta = (BindWidget))
 	class UCanvasPanel* GameplayMenuRootPanel;
 
+	UPROPERTY(meta = (BindWidget))
+	class UCrosshairWidget* CrosshairWidget;
+
 	UPROPERTY(Transient, meta = (BindWidgetAnim))
 	class UWidgetAnimation* ShopPopupAnimation;
 
