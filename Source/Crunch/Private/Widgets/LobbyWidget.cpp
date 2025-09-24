2b59825baf0045d42798de96e26cc82e8aae6f79
losophy <losophyl@gamil.com>
2025-09-24 21:18:27 +0800

    237.237 Setup the Visuals of the Lobby Level

--- Source/Crunch/Private/Widgets/LobbyWidget.cpp
+++ Source/Crunch/Private/Widgets/LobbyWidget.cpp
@@ -188,7 +188,7 @@ void ULobbyWidget::SpawnCharacterDisplay()
 
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-	CharacterDisplay = GetWorld()->SpawnActor<ACharacterDisplay>(CharacterDisplayClass, SpawnParams);
+	CharacterDisplay = GetWorld()->SpawnActor<ACharacterDisplay>(CharacterDisplayClass, CharacterDisplayTransform, SpawnParams);
 	GetOwningPlayer()->SetViewTarget(CharacterDisplay);
 }
 
